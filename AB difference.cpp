0938b299d6f7136308aa81074947095872857ed8
ragipetamahaboobbasha777-wq <ragipetamahaboobbasha777@gmail.com>
2025-12-05 09:10:30 +0530

    Add files via upload

---
+++ AB difference.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> // for abs()
+
+int main() {
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    int sum = A + B;
+    int product = A * B;
+
+    int difference = abs(sum - product);
+
+    printf("%d", difference);
+
+    return 0;
+}
+
