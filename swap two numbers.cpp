8d5b8c2885737d42fa56f8202f6d7d477bb6faec
ragipetamahaboobbasha777-wq <ragipetamahaboobbasha777@gmail.com>
2025-12-05 08:39:17 +0530

    Add files via upload

---
+++ swap two numbers.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // Swap without temporary variable
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
